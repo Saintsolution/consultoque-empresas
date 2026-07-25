4e6f56ae67a5b92bd6e1fc28898af6d3a2794b3e
Saintsolution <saintsolution.inc@gmail.com>
2026-07-25 19:19:33 -0300

    Cria site ConsulToque sem CTA

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,12 @@ import Hero from '@/components/home/Hero';
 import Benefits from '@/components/home/Benefits';
 import Pricing from '@/components/home/Pricing';
 import VideoSection from '@/components/home/VideoSection';
-import CTASection from '@/components/home/CTASection';
 import { LegalCompliance } from '@/components/home/LegalCompliance';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <CTASection />
       <Benefits />
       <Pricing />
       <VideoSection />
